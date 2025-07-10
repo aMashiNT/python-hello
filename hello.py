ff4103a834829eb65eac81b868a47303c595fc89
Ahmed Mashroor <m7intiaf@gmail.com>
2025-07-10 12:09:08 +0600

    Type function code added to hello.py !

--- hello.py
+++ hello.py
@@ -4,3 +4,15 @@ a = 45;
 b = 65;
 
 print(a+b)
+
+a = 25;
+b = "the earth is so big but smaller compared to the universe"
+c = 95.99
+d = True
+
+print(type(a))  # THIS gives the type for a,b,c,d ! ! ! 
+print(type(b))
+print(type(c))
+print(type(d))
+
+
