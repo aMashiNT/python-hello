e25266300a62ffce305a2dc206a3fea566781faf
Ahmed Mashroor <m7intiaf@gmail.com>
2025-07-14 11:40:05 +0600

    learning

--- hello.py
+++ hello.py
@@ -1,24 +1,34 @@
 print("Hello Ahmed! This is my first GitHub Python project.")
 
-a = 45;
-b = 65;
+a = 45
+b = 65
 
 print(a+b)
-
-a = 45;
-b = 56;
-c = 78;
+a = 45
+b = 56
+c = 99
 print(a+b+c)
 
-a = 25;
+a = 25
 b = "the earth is so big but smaller compared to the universe"
 c = 95.99
 d = True
+e = 56
 
 print(type(a))  # THIS gives the type for a !
 print(type(b))  # THIS gives the type for b !
 print(type(c))  # THIS gives the type for c !
 print(type(d))  # THIS gives the type for d !
+print(type(e))  # THIS gives the type for e !
+
+
+
+
+
+
+
+
+
 
 
 
